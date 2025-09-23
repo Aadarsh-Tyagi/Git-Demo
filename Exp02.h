3c8d428f33a6eacfeade5dd28070dd4640f7f7f2
Aadarsh-Tyagi <226226140+Aadarsh-Tyagi@users.noreply.github.com>
2025-09-24 00:08:51 +0530

    Add files via upload

---
+++ Exp02.h
@@ -0,0 +1,19 @@
+//
+//  Exp02.h
+//  Coding
+//
+//  Created by Arsh on 27/08/25.
+//
+
+#include<stdio.h>
+
+def main ()
+{
+   name = input ("What is you name:");
+   age = int(input ("What is your age:"));
+    
+   printf("hello,{name}\n");
+   printf("your {age} is\n");
+
+    return 0:
+}
