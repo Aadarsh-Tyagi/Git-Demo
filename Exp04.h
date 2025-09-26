72211735ac6384709d04d77fe7099e62d02ae434
Aadarsh-Tyagi <226226140+Aadarsh-Tyagi@users.noreply.github.com>
2025-09-26 23:21:00 +0530

    Add files via upload

---
+++ Exp04.h
@@ -0,0 +1,23 @@
+//
+//  Exp04.h
+//  Coding
+//
+//  Created by Arsh on 27/08/25.
+//
+
+#include <studio.h>
+
+int main()
+{
+    float length, width, area, perimeter;
+    printf("Enter the length of the rectangle: ");
+    scanf("%f", &length);
+    printf("Enter the width of the rectangle: ");
+    scanf("%f", &width);
+    area = length * width;
+    perimeter = 2 * (length + width);
+    printf("Area of the rectangle: %.2f\n", area);
+    printf("Perimeter of the rectangle: %.2f\n", perimeter);
+
+    return 0;
+}
