f8103b2f4fd641ba1ca0caa9d2a441f16f3dce85
Aadarsh Tyagi <226226140+Aadarsh-Tyagi@users.noreply.github.com>
2026-02-12 16:01:57 +0530

    Add files via upload

--- Tri01.c
+++ Tri01.c
@@ -1,15 +1,60 @@
+/*
+Matrix Multiplication using Pointers plus functions
+*/
 #include <stdio.h>
 
-int main()
+void multiply(int *A, int *B, int *C, int m, int n, int p) 
 {
-    int age = 30;
-    float sal = 1500.50;
 
-    printf("\nValue of age = %d", age);
-    printf("\nAddress of age = %p", (void *)&age);
+    for(int i = 0; i < m; i++) 
+    {
+        for(int j = 0; j < p; j++) 
+        {
 
-    printf("\nValue of sal = %f", sal);
-    printf("\nAddress of sal = %p", (void *)&sal);
+            *(C + i*p + j) = 0;
+
+            for(int k = 0; k < n; k++) 
+            {
+                *(C + i*p + j) +=  (*(A + i*n + k)) * (*(B + k*p + j));
+            }
+        }
+    }
+}
+
+int main() 
+{
+
+    int m, n, p;
+
+    printf("Enter rows and columns of Matrix A: ");
+    scanf("%d %d", &m, &n);
+
+    printf("Enter columns of Matrix B: ");
+    scanf("%d", &p);
+
+    int A[m][n], B[n][p], C[m][p];
+
+    printf("\nEnter elements of Matrix A:\n");
+    for(int i = 0; i < m; i++)
+        for(int j = 0; j < n; j++)
+            scanf("%d", &A[i][j]);
+
+    printf("\nEnter elements of Matrix B:\n");
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < p; j++)
+            scanf("%d", &B[i][j]);
+
+    multiply(&A[0][0], &B[0][0], &C[0][0], m, n, p);
+
+    printf("\nResult Matrix C:\n");
+    for(int i = 0; i < m; i++) 
+    {
+        for(int j = 0; j < p; j++) 
+        {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
