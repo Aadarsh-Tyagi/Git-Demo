bf7f7495261f386d85401dad4615e01d4710eaec
Aadarsh Tyagi <226226140+Aadarsh-Tyagi@users.noreply.github.com>
2026-02-12 16:02:16 +0530

    Add files via upload

--- Poi01.c
+++ Poi01.c
@@ -1,15 +1,58 @@
 /*
-Make a code using pointers 
+Take two matrices array in pointers and multiply them
 */
 
 #include <stdio.h>
 
 int main ()
 {
-    int list[10] ;
-    int *p;
+    int m, n, p;
 
-    p = list ;
+    printf("Enter rows and columns of Matrix A: ");
+    scanf("%d %d", &m, &n);
 
-    for ()
+    printf("Enter columns of Matrix B: ");
+    scanf("%d", &p);
+
+    int A[m][n], B[n][p], C[m][p];
+
+    printf("\nEnter elements of Matrix A:\n");
+    for(int i = 0; i < m; i++)
+        for(int j = 0; j < n; j++)
+            scanf("%d", &A[i][j]);
+
+    printf("\nEnter elements of Matrix B:\n");
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < p; j++)
+            scanf("%d", &B[i][j]);
+
+    int *ptrA = &A[0][0];
+    int *ptrB = &B[0][0];
+    int *ptrC = &C[0][0];
+
+    for(int i = 0; i < m; i++) 
+    {
+        for(int j = 0; j < p; j++) 
+        {
+
+            *(ptrC + i*p + j) = 0;
+
+            for(int k = 0; k < n; k++) 
+            {
+                *(ptrC + i*p + j) += (*(ptrA + i*n + k)) * (*(ptrB + k*p + j));
+            }
+        }
+    }
+
+    printf("\nResult Matrix C:\n");
+    for(int i = 0; i < m; i++) 
+    {
+        for(int j = 0; j < p; j++) 
+        {
+            printf("%d ", *(ptrC + i*p + j));
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
