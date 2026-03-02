a4302aa217dda98d2a4adc3ff409f7c43893e06c
Aadarsh Tyagi <226226140+Aadarsh-Tyagi@users.noreply.github.com>
2026-03-02 21:15:44 +0530

    Add files via upload

--- Ques02.c
+++ Ques02.c
@@ -1,38 +1,40 @@
-// Array Insertion
+// O(log n)
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-#define MAX 10000
-
-int arr[MAX];
-int n = 5000;
-
-void insert(int pos, int val) {
-    for (int i = n; i > pos; i--)
-        arr[i] = arr[i - 1];
-    arr[pos] = val;
-    n++;
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == key) return mid;
+        else if (arr[mid] < key) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
 }
 
 int main() {
+    int sizes[] = {1000, 10000, 100000, 1000000};
     clock_t start, end;
 
-    for (int i = 0; i < n; i++)
-        arr[i] = i + 1;
+    printf("O(log n) - Binary Search\n");
+    printf("%-12s %-15s\n", "n", "Time(sec)");
+    printf("---------------------------\n");
 
-    start = clock();
-    for (int r = 0; r < 1000; r++)
-        insert(0, 99);
-    end = clock();
-    printf("Insert at beginning: %f sec\n", (double)(end - start) / CLOCKS_PER_SEC);
+    for (int i = 0; i < 4; i++) {
+        int n = sizes[i];
+        int *arr = (int*)malloc(n * sizeof(int));
+        for (int j = 0; j < n; j++) arr[j] = j;
 
-    n = 5000;
-    start = clock();
-    for (int r = 0; r < 1000; r++)
-        insert(n, 99);
-    end = clock();
-    printf("Insert at end: %f sec\n", (double)(end - start) / CLOCKS_PER_SEC);
+        start = clock();
+        for (int r = 0; r < 100000; r++)
+            binarySearch(arr, n, -1);
+        end = clock();
 
+        printf("%-12d %-15f\n", n, (double)(end - start) / CLOCKS_PER_SEC);
+        free(arr);
+    }
     return 0;
 }
