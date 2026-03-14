75eb3e9b16613e48ee232cdde8416183a5373d9a
Aadarsh Tyagi <226226140+Aadarsh-Tyagi@users.noreply.github.com>
2026-03-14 19:43:06 +0530

    Add files via upload

--- Ques01.c
+++ Ques01.c
@@ -1,22 +1,21 @@
-// O(1)
+// Array Traversal
 
 #include <stdio.h>
 #include <time.h>
 
-int getElement(int arr[], int index) {
-    return arr[index];
-}
-
 int main() {
-    int arr[] = {10, 20, 30, 40, 50};
+    int n = 5000;
+    int arr[5000];
     clock_t start, end;
 
+    for (int i = 0; i < n; i++)
+        arr[i] = i + 1;
+
     start = clock();
-    for (int r = 0; r < 1000000; r++)
-        getElement(arr, 3);
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
     end = clock();
 
-    printf("O(1) - Array Access\n");
-    printf("n = fixed, Time = %f sec\n", (double)(end - start) / CLOCKS_PER_SEC);
+    printf("\nTime: %f sec\n", (double)(end - start) / CLOCKS_PER_SEC);
     return 0;
 }
