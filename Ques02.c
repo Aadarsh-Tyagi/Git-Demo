4bd93499231d52462cf8f60706ee42450f17e55b
Aadarsh Tyagi <226226140+Aadarsh-Tyagi@users.noreply.github.com>
2026-03-26 19:18:49 +0530

    Add files via upload

--- Ques02.c
+++ Ques02.c
@@ -1,36 +1,51 @@
-// Array Insertion
-
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-#define MAX 10000
+struct node {
+    int data;
+    struct node *next;
+};
 
-int arr[MAX];
-int n = 5000;
+struct node* insertatbegin(struct node *head, int val) {
+    struct node *newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = val;
+    newnode->next = head;
+    return newnode;
+}
 
-void insert(int pos, int val) {
-    for (int i = n; i > pos; i--)
-        arr[i] = arr[i - 1];
-    arr[pos] = val;
-    n++;
+struct node* insertatend(struct node *head, int val) {
+    struct node *newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = val;
+    newnode->next = NULL;
+    if (head == NULL) return newnode;
+    struct node *temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = newnode;
+    return head;
 }
 
 int main() {
+    struct node *head = NULL;
     clock_t start, end;
 
-    for (int i = 0; i < n; i++)
-        arr[i] = i + 1;
+    for (int i = 0; i < 5000; i++) {
+        struct node *newnode = (struct node*)malloc(sizeof(struct node));
+        newnode->data = i + 1;
+        newnode->next = head;
+        head = newnode;
+    }
 
     start = clock();
     for (int r = 0; r < 1000; r++)
-        insert(0, 99);
+        head = insertatbegin(head, 99);
     end = clock();
     printf("Insert at beginning: %f sec\n", (double)(end - start) / CLOCKS_PER_SEC);
 
-    n = 5000;
     start = clock();
     for (int r = 0; r < 1000; r++)
-        insert(n, 99);
+        head = insertatend(head, 99);
     end = clock();
     printf("Insert at end: %f sec\n", (double)(end - start) / CLOCKS_PER_SEC);
 
