74251d90bc00e97de3a6749bd60d73181d0b39f3
Aadarsh Tyagi <226226140+Aadarsh-Tyagi@users.noreply.github.com>
2026-03-26 19:23:27 +0530

    Add files via upload

--- Ques01.c
+++ Ques01.c
@@ -1,43 +1,71 @@
+// Reverse a singly linked list (iterative method).
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-struct node {
+struct node
+{
     int data;
     struct node *next;
 };
 
-void traversal(struct node *head) {
-    struct node *temp = head;
-    while (temp != NULL) {
-        printf("%d ", temp->data);
-        temp = temp->next;
-    }
-    printf("\n");
+struct node* createNode(int value)
+{
+    struct node *newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
 }
 
-int main() {
-    struct node *head = NULL;
-    struct node *temp;
-    clock_t start, end;
+struct node* reverseList(struct node *head)
+{
+    struct node *prev = NULL;
+    struct node *current = head;
+    struct node *next = NULL;
 
-    for (int i = 0; i < 5000; i++) {
-        struct node *newnode = (struct node*)malloc(sizeof(struct node));
-        newnode->data = i + 1;
-        newnode->next = NULL;
-        if (head == NULL) {
-            head = newnode;
-            temp = head;
-        } else {
-            temp->next = newnode;
-            temp = temp->next;
-        }
+    while(current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
     }
 
-    start = clock();
-    traversal(head);
-    end = clock();
+    return prev;   
+}
 
-    printf("Time: %f sec\n", (double)(end - start) / CLOCKS_PER_SEC);
+void display(struct node *head)
+{
+    struct node *temp = head;
+
+    while(temp != NULL)
+    {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL\n");
+}
+
+int main()
+{
+    struct node *head;
+
+    struct node *n1 = createNode(10);
+    struct node *n2 = createNode(20);
+    struct node *n3 = createNode(30);
+
+    head = n1;
+    n1->next = n2;
+    n2->next = n3;
+
+    printf("Original List:\n");
+    display(head);
+
+    head = reverseList(head);
+
+    printf("Reversed List:\n");
+    display(head);
+
     return 0;
 }
