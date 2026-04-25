a0032fdbdbff71cba93674abeede566c015300c5
Aadarsh Tyagi <226226140+Aadarsh-Tyagi@users.noreply.github.com>
2026-04-25 23:50:09 +0530

    Add files via upload

---
+++ Ques01.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int queue[MAX];
+int front = 0, rear = -1;
+
+// Enqueue
+void enqueue(int value) {
+    if (rear == MAX - 1) {
+        return; // simple overflow handling
+    }
+    rear++;
+    queue[rear] = value;
+}
+
+// Display
+void display() {
+    for (int i = front; i <= rear; i++) {
+        printf("%d ", queue[i]);
+    }
+}
+
+int main() {
+    int n, x;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+
+    display();
+
+    return 0;
+}
