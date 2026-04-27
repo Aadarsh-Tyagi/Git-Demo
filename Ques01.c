6cc7b9cbe38ba89159dbc229663fe49d73ab2e22
Aadarsh Tyagi <226226140+Aadarsh-Tyagi@users.noreply.github.com>
2026-04-27 23:57:46 +0530

    Add files via upload

--- Ques01.c
+++ Ques01.c
@@ -1,22 +1,105 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    int n;
-    scanf("%d", &n);
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
 
-    int arr[n];
-    for (int i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
+// Create node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
 
-    int max = arr[0], min = arr[0];
+// Insert at end
+struct Node* insertEnd(struct Node* head, int value) {
+    struct Node* newNode = createNode(value);
 
-    for (int i = 1; i < n; i++) {
-        if (arr[i] > max) max = arr[i];
-        if (arr[i] < min) min = arr[i];
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+// Get length
+int getLength(struct Node* head) {
+    int count = 0;
+    while (head != NULL) {
+        count++;
+        head = head->next;
+    }
+    return count;
+}
+
+// Rotate right by k
+struct Node* rotateRight(struct Node* head, int k) {
+    if (head == NULL || head->next == NULL || k == 0)
+        return head;
+
+    int n = getLength(head);
+    k = k % n;  // important
+
+    if (k == 0)
+        return head;
+
+    struct Node* temp = head;
+
+    // go to last node
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    // make circular
+    temp->next = head;
+
+    // move to (n-k)th node
+    int steps = n - k;
+    struct Node* newTail = head;
+
+    for (int i = 1; i < steps; i++) {
+        newTail = newTail->next;
     }
 
-    printf("Max: %d\n", max);
-    printf("Min: %d\n", min);
+    struct Node* newHead = newTail->next;
+
+    // break circle
+    newTail->next = NULL;
+
+    return newHead;
+}
+
+// Display
+void display(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    int n, x, k;
+    struct Node* head = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        head = insertEnd(head, x);
+    }
+
+    scanf("%d", &k);
+
+    head = rotateRight(head, k);
+
+    display(head);
 
     return 0;
 }
