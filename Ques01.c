a8bcaa64cebe6853ab26b5b23ec2bbd5b6d8d324
Aadarsh Tyagi <226226140+Aadarsh-Tyagi@users.noreply.github.com>
2026-04-30 23:57:07 +0530

    Add files via upload

--- Ques01.c
+++ Ques01.c
@@ -1,81 +1,133 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-struct Node {
-    int data;
-    struct Node* next;
-};
+#define MAX 100
 
-struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->next = NULL;
-    return newNode;
+int dq[MAX];
+int front = -1, rear = -1;
+
+// Check empty
+int isEmpty() {
+    return (front == -1);
 }
 
-struct Node* insertEnd(struct Node* head, int data) {
-    struct Node* newNode = createNode(data);
-    if (head == NULL) return newNode;
-    struct Node* temp = head;
-    while (temp->next != NULL)
-        temp = temp->next;
-    temp->next = newNode;
-    return head;
+// Check full
+int isFull() {
+    return ((rear + 1) % MAX == front);
 }
 
-// Merge two sorted lists into one sorted list
-struct Node* mergeSorted(struct Node* a, struct Node* b) {
-    // Use a dummy head to simplify edge cases
-    struct Node dummy;
-    dummy.next = NULL;
-    struct Node* tail = &dummy;
+// push_front
+void push_front(int x) {
+    if (isFull()) return;
 
-    while (a != NULL && b != NULL) {
-        if (a->data <= b->data) {
-            tail->next = a;
-            a = a->next;
-        } else {
-            tail->next = b;
-            b = b->next;
-        }
-        tail = tail->next;
+    if (isEmpty()) {
+        front = rear = 0;
+    } else {
+        front = (front - 1 + MAX) % MAX;
     }
-
-    // Attach remaining nodes
-    tail->next = (a != NULL) ? a : b;
-
-    return dummy.next;
+    dq[front] = x;
 }
 
-void traverse(struct Node* head) {
-    int first = 1;
-    while (head != NULL) {
-        if (!first) printf(" ");
-        printf("%d", head->data);
-        head = head->next;
-        first = 0;
+// push_back
+void push_back(int x) {
+    if (isFull()) return;
+
+    if (isEmpty()) {
+        front = rear = 0;
+    } else {
+        rear = (rear + 1) % MAX;
+    }
+    dq[rear] = x;
+}
+
+// pop_front
+void pop_front() {
+    if (isEmpty()) return;
+
+    if (front == rear) {
+        front = rear = -1;
+    } else {
+        front = (front + 1) % MAX;
+    }
+}
+
+// pop_back
+void pop_back() {
+    if (isEmpty()) return;
+
+    if (front == rear) {
+        front = rear = -1;
+    } else {
+        rear = (rear - 1 + MAX) % MAX;
+    }
+}
+
+// get front
+void getFront() {
+    if (isEmpty()) printf("-1\n");
+    else printf("%d\n", dq[front]);
+}
+
+// get back
+void getBack() {
+    if (isEmpty()) printf("-1\n");
+    else printf("%d\n", dq[rear]);
+}
+
+// size
+int getSize() {
+    if (isEmpty()) return 0;
+    return (rear - front + MAX) % MAX + 1;
+}
+
+// display
+void display() {
+    if (isEmpty()) return;
+
+    int i = front;
+    while (1) {
+        printf("%d ", dq[i]);
+        if (i == rear) break;
+        i = (i + 1) % MAX;
     }
     printf("\n");
 }
 
 int main() {
-    int n;
+    int n, x;
+    char op[20];
+
     scanf("%d", &n);
-    struct Node* listA = NULL;
+
     for (int i = 0; i < n; i++) {
-        int val; scanf("%d", &val);
-        listA = insertEnd(listA, val);
+        scanf("%s", op);
+
+        if (op[0] == 'p' && op[5] == 'f') { // push_front
+            scanf("%d", &x);
+            push_front(x);
+        }
+        else if (op[0] == 'p' && op[5] == 'b') { // push_back
+            scanf("%d", &x);
+            push_back(x);
+        }
+        else if (op[0] == 'p' && op[4] == 'f') { // pop_front
+            pop_front();
+        }
+        else if (op[0] == 'p' && op[4] == 'b') { // pop_back
+            pop_back();
+        }
+        else if (op[0] == 'f') { // front
+            getFront();
+        }
+        else if (op[0] == 'b') { // back
+            getBack();
+        }
+        else if (op[0] == 's') { // size
+            printf("%d\n", getSize());
+        }
     }
 
-    int m;
-    scanf("%d", &m);
-    struct Node* listB = NULL;
-    for (int i = 0; i < m; i++) {
-        int val; scanf("%d", &val);
-        listB = insertEnd(listB, val);
-    }
+    // Final state
+    display();
 
-    struct Node* merged = mergeSorted(listA, listB);
-    traverse(merged);
     return 0;
 }
